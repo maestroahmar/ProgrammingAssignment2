cf6fd675127c488dee8ccee2f271e3458c9d3858
maestroahmar <solemn_martinian@yahoo.com>
2016-06-10 21:08:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The first function is meant for getting and setting the original matrix and 
+## inverted matrix where as the second function is used to cache the given matrix or if already chached then retrive the
+## inverted function with out actually calculating it again.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+##This function is meant for getting and setting the original matrix and inverted matrix
+
+makeCacheMatrix <- function(x = matrix())
+{
+inv_ori <- NULL
+seto <- function(y)  x <<- y
+geto <- function()   x
+seti <- function(i)  inv_ori <<- i
+geti <- function()  inv_ori
+list(seto = seto, geto = geto, seti = seti, geti = geti)
 }
 
-
-## Write a short comment describing this function
-
+## function is used to cache the given matrix or if already chached then retrive the
+## inverted function with out actually calculating it again.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$geti()
+        if(!is.null(i)) {
+                message("getting cached Matrix")
+                return(i)
+        }
+        data <- x$geto()
+        i <- solve(data, ...)
+        x$seti(i)
+        i
 }
